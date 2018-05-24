c4dbc177bdc6b60cf93bf5929a24e84593156797
Mariayee <maarisa75@gmail.com>
2018-05-24 11:16:16 +0800

    add comment

--- srs2/settings.py
+++ srs2/settings.py
@@ -112,7 +112,7 @@ USE_I18N = True
 USE_L10N = True
 
 USE_TZ = True
-
+# this program is created by  Pyhthon 
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/2.0/howto/static-files/
